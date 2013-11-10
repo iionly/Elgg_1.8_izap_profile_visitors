c2b45ac9909488f5a513908865de9669eae57fa4
iionly <iionly@gmx.de>
2013-11-10 15:20:04 +0100

    New release; changes see README.txt

---
+++ izap_profile_visitors/languages/de.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * iZAP izap profile visitor
+ *
+ * @license GNU Public License version 3
+ * @author iZAP Team "<support@izap.in>"
+ * @link http://www.izap.in/
+ *
+ * iionly; Version 1.8:
+ * compatibility elgg-1.8
+ */
+
+$german = array(
+'izapProfileVisitor:Widget' => 'Letzte Besucher der Profilseite',
+'izapProfileVisitor:WidgetDescription' => 'Dieses Widget zeigt Dir, wer Deine Profilseite zuletzt besucht hat.',
+'izapProfileVisitor:NumberOfVisitors' => 'Anzahl der anzuzeigenden Besucher',
+'izapProfileVisitor:NoVisits' => 'Es hat mich noch niemand besucht. :(',
+);
+
+add_translation("de",$german);
